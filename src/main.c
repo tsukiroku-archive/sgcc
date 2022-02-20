61405cd21498ae7265563a94f39b5166b5a581c7
rwdv <59119316+rwdv@users.noreply.github.com>
2022-02-20 17:24:42 +0900

    Update `src\main.c` (Example)

---
+++ src/main.c
@@ -0,0 +1,18 @@
+// Example file
+
+/**
+ * 
+ * @file main.c
+ * @author rwdv
+ * @date 2022-02-20
+ * 
+ * @copyright Copyright 2022. rwdv
+ */
+
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+    for (int i = 0; i < argc; i++) {
+        printf("%d: %s\n", i, argv[i]);
+    }
+}
